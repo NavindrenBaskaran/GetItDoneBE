dda8e75be037260428eb969afa7ad6d39b8dd0b8
Navindren <navin25@hotmail.com>
2018-05-11 22:26:29 +0800

    Remove validation concern

--- app/models/concerns/email_validator.rb
+++
@@ -1,9 +0,0 @@
-# Validate the given email format
-class EmailValidator < ActiveModel::Validator
-  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-
-  def validate(record)
-    record.errors.add(:email, 'Invalid Email!') unless record.email =~
-    VALID_EMAIL_REGEX
-  end
-end
